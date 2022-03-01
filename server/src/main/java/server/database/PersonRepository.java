d908f1834f04e43e4c7f00e91185b76775841679
Bryan Wassenaar <b.j.a.wassenaar@student.tudelft.nl>
2022-03-01 19:28:34 +0100

    Deleted all the template server files

--- server/src/main/java/server/database/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package server.database;
-
-import commons.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository <Person, Long> {
-}
