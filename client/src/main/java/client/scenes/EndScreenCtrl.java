1ee3b162c858171c16407f6dc203807546355548
Tom Kitak <t.kitak@student.tudelft.nl>
2022-03-01 22:36:51 +0100

    Wiring buttons and making base for controllers
    I made FXML template and wired the pages so that you can go from one page to another

--- client/src/main/java/client/scenes/EndScreenCtrl.java
+++ client/src/main/java/client/scenes/EndScreenCtrl.java
@@ -1,4 +1,25 @@
 package client.scenes;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import com.google.inject.Inject;
+import client.utils.ServerUtils;
+
 public class EndScreenCtrl {
+
+
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+
+    @Inject
+    public EndScreenCtrl(ServerUtils server, MainCtrl mainCtrl) {
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+    }
+
+    @FXML
+    void returnToHomePagePressed(ActionEvent event) {
+        mainCtrl.showHomeScreen();
+    }
+
 }
--- client/src/main/java/client/scenes/QuizScreenCtrl.java
+++ client/src/main/java/client/scenes/QuizScreenCtrl.java
@@ -1,4 +1,75 @@
 package client.scenes;
 
-public class QuizScreenCtrl {
+import java.net.URL;
+import java.util.ResourceBundle;
+import com.google.inject.Inject;
+import client.utils.ServerUtils;
+import commons.Question;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class QuizScreenCtrl implements Initializable {
+
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+
+    @FXML
+    private Button buttonR01C0;
+
+    @FXML
+    private Button buttonR0C0;
+
+    @FXML
+    private Button buttonR0C1;
+
+    @FXML
+    private Button buttonR1C1;
+
+    @FXML
+    private Text questionField;
+
+    @Inject
+    public QuizScreenCtrl(ServerUtils server, MainCtrl mainCtrl) {
+        this.server = server;
+        this.mainCtrl = mainCtrl;
+    }
+
+    /**
+     * Here we initialise placeholder for question text with actual question.
+     * Buttons are also initialised with values here
+     * @param location
+     * @param resources
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Question question = server.getQuestion();
+    }
+
+    /**
+     * Main ctrl used here only for demonstration
+     * @param event
+     */
+    @FXML
+    void pressedR0C0(ActionEvent event) {
+        mainCtrl.showEndScreen();
+    }
+
+    @FXML
+    void pressedR0C1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pressedR1C0(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pressedR1C1(ActionEvent event) {
+
+    }
+
 }
