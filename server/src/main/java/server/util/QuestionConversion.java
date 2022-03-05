30bab5800ebc7c6199b875bd2a902fcc8c506e3e
Bryan Wassenaar <b.j.a.wassenaar@student.tudelft.nl>
2022-03-05 17:59:26 +0100

    Changed Question implementation
    - Made Question.java abstract.
    - Added CompareQuestion.java as question type.
    - Implemented Activity to Question conversion.
    - Renamed QuestionTest.java to CompareQuestionTest.java.
    - Changed Tests in CompareQuestionTest.java

--- server/src/main/java/server/util/QuestionConversion.java
+++ server/src/main/java/server/util/QuestionConversion.java
@@ -1,11 +1,35 @@
 package server.util;
 
+import commons.CompareQuestion;
 import commons.Question;
 import server.Activity;
 
 public class QuestionConversion {
 
+    /**
+     * Method to convert activities to a Question.
+     * @param activities The activities to be used.
+     * @return A Question.
+     */
     public static Question convertActivity(Activity[] activities) {
+        if (activities != null && activities.length == 4){
+            String[] titles = new String[4];
+            int[] wattages = new int[4];
+            for (int i = 0; i < 4; i++){
+                if (activities[i] != null) {
+                    Activity current = activities[i];
+                    if (current.getTitle() != null && current.getWh() != 0) {
+                        titles[i] = current.getTitle();
+                        wattages[i] = current.getWh();
+                    } else {
+                        return null;
+                    }
+                } else {
+                    return null;
+                }
+            }
+            return new CompareQuestion(titles, wattages);
+        }
         return null;
     }
 }
