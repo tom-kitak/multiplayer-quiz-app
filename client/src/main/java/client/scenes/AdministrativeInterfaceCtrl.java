97cd133d8c12877e07e79358c977ddeefacc17e7
Tom Kitak <t.kitak@student.tudelft.nl>
2022-03-10 22:09:19 +0100

    FXML file

--- client/src/main/java/client/scenes/AdministrativeInterfaceCtrl.java
+++ client/src/main/java/client/scenes/AdministrativeInterfaceCtrl.java
@@ -1,4 +1,18 @@
 package client.scenes;
 
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+
 public class AdministrativeInterfaceCtrl {
+
+    @FXML
+    void addActivityPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backPressed(ActionEvent event) {
+
+    }
+
 }
