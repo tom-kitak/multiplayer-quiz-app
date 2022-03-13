6ceca8d1e44f904c1d4282cf9473ac5b0c1be04f
ebulboaca <A.Bulboaca@student.tudelft.nl>
2022-03-13 22:54:10 +0100

    Added Leaderboard

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -24,6 +24,8 @@ import org.glassfish.jersey.client.ClientConfig;
 
 import server.Score;
 
+import java.util.List;
+
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class ServerUtils {
@@ -51,5 +53,12 @@ public class ServerUtils {
                 .request(APPLICATION_JSON)
                 .accept(APPLICATION_JSON)
                 .post(Entity.entity(score, APPLICATION_JSON), Score.class);
+    }
+    public List<Score> getAllScores() {
+        return ClientBuilder.newClient(new ClientConfig())
+                .target(SERVER).path("/api/score/get")
+                .request(APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
+                .get(new GenericType<List<Score>>() {});
     }
 }
