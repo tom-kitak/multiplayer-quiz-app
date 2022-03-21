c9599ef28a6ceca65f02422c1e5f738fc8a8dc83
Bryan Wassenaar <b.j.a.wassenaar@student.tudelft.nl>
2022-03-21 12:18:03 +0100

    Fixed EndScreenCtrl.java causing error on startup.

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -132,7 +132,7 @@ public class MainCtrl {
     public void showEndScreen() {
         primaryStage.setTitle("End Screen");
         primaryStage.setScene(endScreen);
-
+        endScreenCtrl.showLeaderboard();
     }
 
     public void showHowToPlay() {
