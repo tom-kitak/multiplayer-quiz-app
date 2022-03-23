e5c6ff6046bb95bc0755f103c9b5e84912c63e6f
Tom Kitak <t.kitak@student.tudelft.nl>
2022-03-23 22:04:28 +0100

    emojiController save

---
+++ server/src/main/java/server/api/EmojiController.java
@@ -0,0 +1,11 @@
+package server.api;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmojiController {
+
+}
