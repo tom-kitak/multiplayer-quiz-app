706b0ad1d9765f58e35c3cfd059d1039d68f2a86
Laurens Michielsen <L.L.Michielsen@student.tudelft.nl>
2022-03-26 14:40:42 +0100

    Fixing of the build

--- client/src/main/java/client/scenes/HomeScreenCtrl.java
+++ client/src/main/java/client/scenes/HomeScreenCtrl.java
@@ -96,8 +96,8 @@ public class HomeScreenCtrl {
      * @param points The points to assign to the score object
      * @return A new score object with the specified points
      */
-    Score getNewScore(int points){
-        Score score = new Score(points, nameField.getText());
+    Score getNewScore(int points, String name){
+        Score score = new Score(points,name);
         return score;
     }
 
