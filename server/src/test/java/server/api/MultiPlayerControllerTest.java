12132dc5d778939595c33c24a03e0fc2974f7832
Tom Kitak <t.kitak@student.tudelft.nl>
2022-04-01 19:50:19 +0200

    Multiplayer controller testing

--- server/src/test/java/server/api/MultiPlayerControllerTest.java
+++ server/src/test/java/server/api/MultiPlayerControllerTest.java
@@ -1,121 +1,86 @@
 package server.api;
 
+import commons.Emoji;
 import commons.MultiGame;
 import commons.Player;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.simp.stomp.StompFrameHandler;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.SockJsClient;
-import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import server.database.ActivityRepository;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
-import java.util.function.Consumer;
+import java.util.Random;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-@SpringBootTest
 public class MultiPlayerControllerTest {
 
-    private static StompSession session;
+    MultiPlayerController ctrl;
 
-    private static StompSession connect(String url) {
-        var client = new StandardWebSocketClient();
-        var stomp = new WebSocketStompClient(client);
-        stomp.setMessageConverter(new MappingJackson2MessageConverter());
-        try {
-            return stomp.connect(url, new StompSessionHandlerAdapter() {}).get();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        } catch (ExecutionException e) {
-            throw new RuntimeException();
-        }
-        throw new IllegalStateException();
-    }
+    @MockBean
+    ActivityRepository repo;
 
-    public static <T> void registerForMessages(String dest, Class<T> type, Consumer<T> consumer) {
-        session.subscribe(dest, new StompFrameHandler() {
-            @Override
-            public Type getPayloadType(StompHeaders headers) {
-                return type;
-            }
-
-            @SuppressWarnings("unchecked")
-            @Override
-            public void handleFrame(StompHeaders headers, Object payload) {
-                consumer.accept((T) payload);
-            }
-        });
-    }
-
-    public static void send(String dest, Object o) {
-        session.send(dest, o);
-    }
+    @MockBean
+    MockRandom random;
 
     @BeforeEach
-    public void setup() {
-        session = connect("ws://localhost:8080/websocket");
+    void setUp() {
+        ctrl = new MultiPlayerController(new Random(), repo);
     }
 
     @Test
-    public void verifyPlayerInWaitingRoom() throws Exception {
-
-        BlockingQueue<MultiGame> blockingQueue = new ArrayBlockingQueue(1);
-
-        registerForMessages("/topic/multi", MultiGame.class, game -> {
-            blockingQueue.add((MultiGame) game);
-        });
-
-        Player player1 = new Player("player1");
-        MultiGame expectedGame = new MultiGame(null);
-        ArrayList<Player> listOfPlayers = new ArrayList<>();
-        listOfPlayers.add(player1);
-        expectedGame.setPlayers(listOfPlayers);
-
-        session.send("/app/multi", player1);
-
-//        assertEquals(expectedGame, blockingQueue.poll(1, SECONDS));
-        assertEquals(1, 1);
+    @DisplayName("Checks whether the /app/multi/ returns game when single player in waiting room")
+    void connectOneTest() {
+        Player player1 = new Player("Joe");
+        MultiGame expected = new MultiGame(null);
+        ArrayList<Player> expectedList = new ArrayList<>();
+        expectedList.add(player1);
+        expected.setPlayers(expectedList);
+        assertEquals(expected, ctrl.connect(player1));
     }
 
-//    @Test
-//    public void verifyWelcomeMessageIsSent() throws Exception {
-//        CountDownLatch latch = new CountDownLatch(1);
-//
-//        webSocketStompClient.setMessageConverter(new MappingJackson2MessageConverter());
-//
-//        StompSession session = webSocketStompClient
-//                .connect(getWsPath(), new StompSessionHandlerAdapter() {
-//                })
-//                .get(1, SECONDS);
-//
-//        session.subscribe("/app/chat", new StompFrameHandler() {
-//
-//            @Override
-//            public Type getPayloadType(StompHeaders headers) {
-//                return Message.class;
-//            }
-//
-//            @Override
-//            public void handleFrame(StompHeaders headers, Object payload) {
-//                latch.countDown();
-//            }
-//        });
-//
-//        if (!latch.await(1, TimeUnit.SECONDS)) {
-//            fail("Message not received");
-//        }
-//    }
+    @Test
+    @DisplayName("Checks whether the /app/multi/ returns game when two players in waiting room")
+    void connectTwoTest() {
+        Player player1 = new Player("Joe");
+        Player player2 = new Player("Donald");
+        MultiGame expected = new MultiGame(null);
+        ArrayList<Player> expectedList = new ArrayList<>();
+        expectedList.add(player1);
+        expectedList.add(player2);
+        expected.setPlayers(expectedList);
+        ctrl.connect(player1);
+        MultiGame game2 = ctrl.connect(player2);
+        assertEquals(expected, game2);
+    }
+
+    @Test
+    @DisplayName("Checks whether the /app/multi/ returns game when two players connect one disconnect in waiting room")
+    void connectTwoAndOneDisconnectTest() {
+        Player player1 = new Player("Joe");
+        Player player2 = new Player("Donald");
+        MultiGame expected = new MultiGame(null);
+        ArrayList<Player> expectedList = new ArrayList<>();
+        expectedList.add(player2);
+        expected.setPlayers(expectedList);
+        ctrl.connect(player1);
+        ctrl.connect(player2);
+        assertEquals(expected, ctrl.connect(player1));
+    }
+
+    @Test
+    @DisplayName("Correct emoji returned")
+    void emojiHandlerTest() {
+        Emoji expected = new Emoji("happy");
+        assertEquals(expected, ctrl.emojiHandler("happy", new Emoji("happy")));
+    }
+
+    @Test
+    @DisplayName("Incorrect emoji returned")
+    void emojiHandlerNotCorrectTest() {
+        Emoji expected = new Emoji("happy");
+        assertNotEquals(expected, ctrl.emojiHandler("happy", new Emoji("sad")));
+    }
 }
